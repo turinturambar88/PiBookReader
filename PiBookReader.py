c94f7cb1df4a2f3b9eec0c6f1827cac28194aaad
Zach <zjonesengineering@gmail.com>
2014-07-28 05:35:49 +0000

    decode hex

--- PiBookReader.py
+++ PiBookReader.py
@@ -1,15 +1,21 @@
 from serial import Serial
 import time
 
-sp = Serial("/dev/ttyAMA0", 9600, timeout=2)
+sp = Serial("/dev/ttyAMA0", 9600, timeout=5)
 if (sp.isOpen() == False):
     sp.open()
 
 sp.flushInput()
 
 while 1:
-    time.sleep(5)
-    data = sp.read(sp.inWaiting())
+    time.sleep(4)
+    
+    sp.flushInput()
+    data = sp.read(14)
+    
+    #data = sp.read(sp.inWaiting())
     print "data =  " + data
+    print "data length = " + str(len(data))
+    print "data hex = " + data.encode('hex')
 
 sp.close()
